a26d372df82c76ef480ad67becdb68295a4dfdf4
shivanisingh03-03-2003 <79415472+shivanisingh03-03-2003@users.noreply.github.com>
2021-11-16 15:55:32 +0530

    Update greads.py

--- greads.py
+++ greads.py
@@ -17,3 +17,23 @@ elif percentage>=40:
     print("Grade E")
 elif percentage<=40:
     print("Grade F")
+    
+    
+    
+  
+
+
+print("enter marks")
+marks=int(input("enter your marks "))
+if marks<25:
+    print("F")
+elif marks>=25 and marks<45:
+    print("E")
+elif marks>=45 and marks<50:
+    print("D")
+elif marks>=50 and marks<=60:
+    print("C")
+elif marks>=60 and marks<=80:
+    print("B")
+else:
+    print("A")
